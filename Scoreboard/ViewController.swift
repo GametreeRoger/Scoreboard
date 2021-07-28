6806ae6cb91c50a79ff9aa5ad4a96375db1799a7
Roger Chang <d9352676@gmail.com>
2021-07-28 11:16:41 +0800

    Initial Commit

---
+++ Scoreboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scoreboard
+//
+//  Created by 張又壬 on 2021/7/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
